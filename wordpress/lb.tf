a80aa35323c9fa963e82e2a400a0500f4e1bf5ed
Demofont <artemios.arthur@gmail.com>
2021-06-13 16:54:04 +0300

    everything working besides ansible

--- wordpress/lb.tf
+++ wordpress/lb.tf
@@ -10,35 +10,6 @@ resource "aws_lb" "lb" {
   }
 }
 
-resource "aws_lb_listener" "lb_l_http" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "lb_l_https" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.httpscertificate
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wb_lb_tg.arn
-  }
-}
-
 resource "aws_lb_target_group" "wb_lb_tg" {
   name     = "wblgtb"
   port     = 80
